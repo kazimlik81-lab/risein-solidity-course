758414e0d6a50ff4a596242f473e7602ee90f3af
kazimlik81-lab <kazimlik81@gmail.com>
2026-09-10 23:21:39 +0400

    Add first task counter contract
    Implement a counter contract with increment, decrement, and retrieval functions.

---
+++ contracts/tasks/01-Counter.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract Example {
+    address owner;
+
+    struct Counter {
+        uint256 number;
+        string description;
+    }
+
+    Counter counter;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can increment or decrement the counter");
+        _;
+    }
+
+    constructor(uint256 initial_value, string memory description) {
+        owner = msg.sender;
+        counter = Counter(initial_value, description);
+    }
+
+    function increment_counter() external onlyOwner {
+        counter.number += 1;
+    }
+
+    function decrement_counter() external onlyOwner {
+        counter.number -= 1;
+    }
+
+    function get_counter_value() external view returns (uint256) {
+        return counter.number;
+    }
+
+    function get_counter_description() external view returns (string memory) {
+        return counter.description;
+    }
+}
